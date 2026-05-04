7c03b2d3e79c88b5156322e80f44c121af063764
adelinekrishna <akrishna981@student.fuhsd.org>
2026-05-04 09:02:30 -0700

    Create Goal.java

---
+++ Goal.java
@@ -0,0 +1,14 @@
+public class Goal
+{
+    private Rectangle bounds;
+
+    public Goal(Rectangle bounds){
+
+    }
+    public boolean goalCollision(Puck puck){
+
+    }
+    public boolean wallCollision(Puck puck){
+        
+    }
+}
