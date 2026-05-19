09429100bd19184c93c05609f1617f604cab2e53
Bunnypower <bunnypower2k20@gmail.com>
2026-05-19 08:41:40 -0700

    reset functionality for puck

--- Puck.java
+++ Puck.java
@@ -5,16 +5,23 @@ import java.awt.Rectangle;
 public class Puck extends Actor
 {
     private float charge;
-    private static final MASS;
+    private static final float MASS = 1;
     private Vector2 position;
+    private Vector2 initialPosition;
 
     public Puck(float charge, Vector2 position)
     {
 
+        initialPosition = position;
     }
     
     public Rectangle collisionBox()
     {
         
+    }
+
+    public void reset()
+    {
+        position = initialPosition;
     }
 }
