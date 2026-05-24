b18780eded7ef10a11d1de2946466729ee888316
Bunnypower <bunnypower2k20@gmail.com>
2026-05-24 00:25:33 -0700

    have total fields not implement the field interfaces

--- TotalBField.java
+++ TotalBField.java
@@ -2,18 +2,13 @@
 /**
  * 
  */
-public class TotalBField extends Actor implements HasBField
+public class TotalBField extends Actor
 {
     private Vector3[][] fieldDisplayPoints;
 
     public void recalculateDisplays()
     {
         
-    }
-
-    public Vector3 getFieldAt(Vector2 position){
-        Vector3 vector = new Vector3();
-        return vector; /*FIX THIS */
     }
 
     @Override
--- TotalEField.java
+++ TotalEField.java
@@ -2,18 +2,13 @@
 /**
  * 
  */
-public class TotalEField extends Actor implements HasEField
+public class TotalEField extends Actor
 {
     private Vector2[][] fieldDisplayPoints;
     
     public void recalculateDisplays()
     {
 
-    }
-
-    public Vector2 getFieldAt(Vector2 position){
-        Vector2 vector = new Vector2();
-        return vector; /*FIX THIS */
     }
 
     @Override
