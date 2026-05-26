e1986286700bd8a3daa05b8716697dc9fae23f4b
roachfest <ashah711@student.fuhsd.org>
2026-05-25 18:35:31 -0700

    Update TitleScene.java

--- TitleScene.java
+++ TitleScene.java
@@ -5,13 +5,42 @@ import java.awt.Graphics2D;
  */
 public class TitleScene extends Scene
 {
-    public void update()
+    private static final int BUTTON_X = 300;
+    private static final int BUTTON_Y = 300;
+    private static final int BUTTON_W = 200;
+    private static final int BUTTON_H = 50;
+    
+    public TitleScene()
     {
-
+        addMouseListener(
+            new MouseAdapter(){
+                public void mouseClicked(MouseEvent e){
+                    if (e.getX() >= BUTTON_X && e.getX() <= BUTTON_X + BUTTON_W && e.getY() >= BUTTON_Y && e.getY() <= BUTTON_Y + BUTTON_H)
+                        Game.instance().pushScene(new LevelSelectScene());
+                }
+            }
+        );
     }
+
+
+    // public void update()
+    // {
+
+    // }
 
     public void render(Graphics2D g)
     {
-        
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 48));
+        g.drawString("Electric Field Hockey", 100, 150);
+
+        g.setColor(Color.WHITE);
+        g.fillRect(BUTTON_X, BUTTON_Y, BUTTON_W, BUTTON_H);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.PLAIN, 24));
+        g.drawString("Level Select", BUTTON_X + 25, BUTTON_Y + 33);
     }
 }
