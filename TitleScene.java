c6fde32be8fa63e4831ed7040b68e706c157417f
roachfest <ashah711@student.fuhsd.org>
2026-05-25 18:59:41 -0700

    Update TitleScene.java

--- TitleScene.java
+++ TitleScene.java
@@ -1,46 +1,39 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 /**
  * 
  */
 public class TitleScene extends Scene
 {
-    private static final int BUTTON_X = 300;
-    private static final int BUTTON_Y = 300;
-    private static final int BUTTON_W = 200;
-    private static final int BUTTON_H = 50;
-    
+
+    private UIButton levelSelectButton;
+
     public TitleScene()
     {
-        addMouseListener(
-            new MouseAdapter(){
-                public void mouseClicked(MouseEvent e){
-                    if (e.getX() >= BUTTON_X && e.getX() <= BUTTON_X + BUTTON_W && e.getY() >= BUTTON_Y && e.getY() <= BUTTON_Y + BUTTON_H)
-                        Game.instance().pushScene(new LevelSelectScene());
-                }
-            }
-        );
+        Rectangle bounds = new Rectangle(300, 300, 200, 50);
+        Runnable action = () -> Game.instance().pushScene(new LevelSelectScene());
+        levelSelectButton = new UIButton(bounds, "Level Select", Color.WHITE, action);
     }
 
+    @Override
+    public void update()
+    {
+        levelSelectButton.update();
+    }
 
-    // public void update()
-    // {
-
-    // }
-
+    @Override
     public void render(Graphics2D g)
     {
         g.setColor(Color.BLACK);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        g.fillRect(0, 0, Game.instance().getWidth(), Game.instance().getHeight());
 
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 48));
         g.drawString("Electric Field Hockey", 100, 150);
 
-        g.setColor(Color.WHITE);
-        g.fillRect(BUTTON_X, BUTTON_Y, BUTTON_W, BUTTON_H);
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Arial", Font.PLAIN, 24));
-        g.drawString("Level Select", BUTTON_X + 25, BUTTON_Y + 33);
+        levelSelectButton.render(g);
     }
 }
