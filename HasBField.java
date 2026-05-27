3e0a21d13ed130cf4cbfbc2d8809493a95cb4474
Bunnypower <bunnypower2k20@gmail.com>
2026-05-26 18:41:33 -0700

    describing the units of field strength

--- HasBField.java
+++ HasBField.java
@@ -4,5 +4,12 @@
  */
 public interface HasBField
 {
+    /**
+     * Gets the 3D vector representing the magnetic field strength due to the
+     * object at a given position.
+     * @param position The world-space position for which to get the magnetic
+     * field strength, in meters
+     * @return The magnetic field strength, in teslas
+     */
     public Vector3 getFieldAt(Vector2 position);
 }
--- HasEField.java
+++ HasEField.java
@@ -4,5 +4,12 @@
  */
 public interface HasEField
 {
+    /**
+     * Gets the vector representing the electric field strength due to the
+     * object at a given position.
+     * @param position The world-space position for which to get the electric
+     * field strength, in meters
+     * @return The electric field strength, in N/C
+     */
     public Vector2 getFieldAt(Vector2 position);
 }
